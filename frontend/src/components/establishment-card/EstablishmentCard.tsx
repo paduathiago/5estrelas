b1ab679fe40dd2f99f0db340a03eead96a754d73
janderson.santos <janderson.santos@sydle.com>
2024-07-01 16:33:43 -0300

    correção card

--- frontend/src/components/establishment-card/EstablishmentCard.tsx
+++ frontend/src/components/establishment-card/EstablishmentCard.tsx
@@ -1,9 +1,9 @@
 import {
-    Card,
-    CardContent,
-    CardFooter,
-    CardHeader,
-    CardTitle,
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 } from "../ui/card";
 import Stars from "../stars/Stars";
 import ImageContainer from "../image-container/ImageContainer";
@@ -14,52 +14,61 @@ import { useLocation } from "react-router-dom";
 import { EstablishmentType } from "@/backTypes";
 
 function truncateToDecimals(number: number) {
-    if (!number) return;
-    const factor = Math.pow(10, 2);
-    return Math.trunc(number * factor) / factor;
+  if (!number) return;
+  const factor = Math.pow(10, 2);
+  return Math.trunc(number * factor) / factor;
 }
 
 function EstablishmentCard({ ...props }: EstablishmentType) {
-    const href = `/establishments/${props.category}/${props.id}`;
+  const href = `/establishments/${props.category}/${props.id}`;
 
-    const [fav, setFav] = useState(props.favorited);
+  const [fav, setFav] = useState(props.favorited);
 
-    function handleFavouriteCLick(
-        ev: React.MouseEvent<SVGSVGElement, MouseEvent>
-    ): void {
-        ev.preventDefault();
+  function handleFavouriteCLick(
+    ev: React.MouseEvent<SVGSVGElement, MouseEvent>
+  ): void {
+    ev.preventDefault();
 
-        favoriteEstablishment(props.id, !fav);
-        setFav(!fav);
-    }
+    favoriteEstablishment(props.id, !fav);
+    setFav(!fav);
+  }
 
-    const image = props.mainImage ? JSON.parse(props.mainImage) : undefined;
-    return (
-        <a href={href}>
-            <Card>
-                <CardHeader className='flex items-center justify-between flex-row'>
-                    <CardTitle>{props.name}</CardTitle>
-                    <Star fill={fav ? "#FFBF00" : "white"} onClick={(ev) => handleFavouriteCLick(ev)}></Star>
-                </CardHeader>
-                <CardContent>
-                    <div className='flex flex-row gap-8'>
-                        <div className='flex flex-row'>
-                            <ImageContainer src={image?.base64} alt="Estabelecimento" className='w-40 h-40' />
-                        </div>
-                        <div className='flex flex-col gap-4'>
-                            <p>{props.description}</p>
-                            <p>{props.category}</p>
-                            <p>{props.description}</p>
-                        </div>
-                    </div>
-
-                </CardContent>
-                <CardFooter>
-                    <Stars score={props.rating} /> ({truncateToDecimals(props.rating)}) {props.numberOfReviews === 0 && 'Ainda não avaliado'}
-                </CardFooter>
-            </Card>
-        </a>
-    );
+  const image = props.mainImage ? JSON.parse(props.mainImage) : undefined;
+  return (
+    <a href={href}>
+      <Card>
+        <CardHeader className="flex items-center justify-between flex-row">
+          <CardTitle>{props.name}</CardTitle>
+          <Star
+            fill={fav ? "#FFBF00" : "white"}
+            onClick={(ev) => handleFavouriteCLick(ev)}
+          ></Star>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-row gap-8">
+            <div className="flex flex-row items-center">
+              <ImageContainer
+                src={image?.base64}
+                alt="Estabelecimento"
+                className="w-40      h-40"
+              />
+            </div>
+            <div className="flex flex-col gap-4">
+              <strong>Funcionamento:</strong>
+              {" " + props?.workingHours} <strong>Telefone: </strong>{" "}
+              {props?.phone}
+              <strong>Endereço: </strong> {props?.address}
+              <strong>Descrição: </strong> {props.description}
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Stars score={props.rating} /> ({truncateToDecimals(props.rating)}){" "}
+          {props.numberOfReviews === 0 && "Ainda não avaliado"}
+        </CardFooter>
+      </Card>
+    </a>
+  );
 }
 
 export default EstablishmentCard;
