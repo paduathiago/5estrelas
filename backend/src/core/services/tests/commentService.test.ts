a960fb53760855513a4b8935d658c4cbb3897330
Thiago Padua <paduathiago@outlook.com>
2024-07-16 19:43:28 -0300

    test comment

---
+++ backend/src/core/services/tests/commentService.test.ts
@@ -0,0 +1,18 @@
+import { CommentService } from '../commentService';
+
+export const commentService = new CommentService();
+
+test('Comment created returns the correct comment', async () => {
+    const commentCreated = await commentService.createComment('1000', 'test-comment');
+    expect(commentCreated.comment).toBe('test-comment');
+})
+
+test('Inserting random id doesnt return a comment', async () => {
+    const commentCreated = await commentService.getComment('randomId');
+    expect(commentCreated).toBe(undefined);
+})
+
+test('Getting comments by review', async () => {
+    const comments = await commentService.getCommentsByReview('1000');
+    expect(comments[0]).toBe('1000');
+})
