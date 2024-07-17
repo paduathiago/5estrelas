6ac970b2918a669ea354075de3a3703a00fb9ce7
gabrielmmf <gabrielmmfialho@hotmail.com>
2024-07-17 14:49:26 -0300

    testando

--- backend/src/core/services/establishmentService.ts
+++ backend/src/core/services/establishmentService.ts
@@ -49,6 +49,7 @@ export class EstablishmentService {
         const numberOfReviews = establishment.numberOfReviews
         const newGeneralRating = ((oldRating * numberOfReviews) + newRating) / (numberOfReviews + 1);
 
+        console.log('ok');
         await this.establishmentRepository.updateRatingOnDb(id, newGeneralRating)
     }
 
